e1addc38eb5ad5a30618d9b59e44c5d4f071e851
anyanglai <1017615366@qq.com>
2019-09-16 14:55:16 +0800

    完成了订单控制器的编写.

---
+++ src/main/java/com/bdqn/controller/BillController.java
@@ -0,0 +1,16 @@
+package com.bdqn.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * ClassName: BillController$
+ * Description:订单控制器
+ * Author: anyanglai
+ * Date: 2019/9/16
+ * Time: 14:51
+ */
+@Controller
+@RequestMapping("/bill")
+public class BillController {
+}
