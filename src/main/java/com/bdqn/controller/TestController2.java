7d2f7d72cb51415b1b7f1e0b383f414952ee8829
anyanglai <1017615366@qq.com>
2019-09-16 15:06:34 +0800

    .ceshi

---
+++ src/main/java/com/bdqn/controller/TestController2.java
@@ -0,0 +1,11 @@
+package com.bdqn.controller;
+
+/**
+ * ClassName: TestController2$
+ * Description:
+ * Author: anyanglai
+ * Date: 2019/9/16
+ * Time: 15:05
+ */
+public class TestController2 {
+}
